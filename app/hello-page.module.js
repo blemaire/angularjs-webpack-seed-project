7f83f018df573f7cdd4f78b5c9663056d46b8a60
Bastien Lemaire <bastien.lemaire@gmail.com>
2019-08-23 15:03:45 +0000

    redundant file

--- app/hello-page.module.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-
-export const module = angular
-    .module('helloPage', [])
-    .component('helloPageComponent', {
-        template: '<h3>hello page world! blah</h3>'
-    })
-;
