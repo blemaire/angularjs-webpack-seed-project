23da91879560cb799a9242d46493c373b88a966c
Bastien Lemaire <bastien.lemaire@gmail.com>
2019-08-26 06:15:49 +0000

    Moved routing to a separate module

--- app/app-routing.module.js
+++ app/app-routing.module.js
@@ -1,4 +1,30 @@
 import angular from 'angular';
 
-export const module = angular.module('appModule', ['ui.router'])
-    
+import {HelloPageModule} from './hello-page/hello-page.module';
+
+export const AppRoutingModule = angular
+    .module('AppRoutingModule', [
+        'ui.router',
+        HelloPageModule.name,
+    ])
+    .config(['$urlRouterProvider', '$locationProvider', function($urlRouterProvider, $locationProvider) {
+        //$locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise('/');
+    }])
+    .config(['$stateProvider', function($stateProvider) {
+        var helloState = {
+            name: 'hello',
+            url: '/hello',
+            component: 'helloPageComponent',
+        }
+
+        var aboutState = {
+            name: 'about',
+            url: '/about',
+            template: '<h3>Its the UI-Router hello world app!</h3>'
+        }
+
+        $stateProvider.state(helloState);
+        $stateProvider.state(aboutState);
+    }])
+    ;
--- app/index.js
+++ app/index.js
@@ -1,36 +1,16 @@
 import angular from 'angular';
 import { Visualizer } from '@uirouter/visualizer';
 
-import {HelloPageModule} from './hello-page/hello-page.module';
+import {AppRoutingModule} from './app-routing.module';
 
 angular
     .module('App',[
         'ngMaterial',
         'ui.router',
-        HelloPageModule.name,
+        AppRoutingModule.name,
     ])
     .run(['$uiRouter', function($uiRouter) {
         var pluginInstance = $uiRouter.plugin(Visualizer);
-    }])
-    .config(['$urlRouterProvider', '$locationProvider', function($urlRouterProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $urlRouterProvider.otherwise('/');
-    }])
-    .config(['$stateProvider', function($stateProvider) {
-        var helloState = {
-            name: 'hello',
-            url: '/hello',
-            component: 'helloPageComponent',
-        }
-
-        var aboutState = {
-            name: 'about',
-            url: '/about',
-            template: '<h3>Its the UI-Router hello world app!</h3>'
-        }
-
-        $stateProvider.state(helloState);
-        $stateProvider.state(aboutState);
     }])
     .controller('AppController', function() {
         this.message = 'Hello. My First AngularJS App';
